e6f63ed89536a6b376089d8600b1fa4e90e6eafb
PippoZord <pippolor8@gmail.com>
2022-12-03 16:50:53 +0100

    ROSSO: ReadFile

---
+++ src/main/java/org/example/Facebuk.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.StringReader;
+
+public class Facebuk {
+    public Facebuk(StringReader input) {
+
+    }
+}
--- src/test/java/FacebukTest.java
+++ src/test/java/FacebukTest.java
@@ -1,2 +1,21 @@
+import org.example.Facebuk;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+
+@Timeout(2)
 public class FacebukTest {
+
+    private final static String input = "Mario Roberta Luca Filippo\nRoberta Mario Luca Filippo Anna\nLuca Mario Roberta Filippo Anna\nFilippo Mario Roberta Luca Anna\nAnna Roberta Luca Filippo\n";
+    @Test
+    public void Storia1_2(){
+        Facebuk SUT = new Facebuk(new StringReader(input));
+        assertThat(SUT.toString()).isEqualTo("Mario Roberta Luca Filippo \nRoberta Mario Luca Filippo Anna \nLuca Mario Roberta Filippo Anna \nFilippo Mario Roberta Luca Anna \nAnna Roberta Luca Filippo \n");
+    }
+
 }
