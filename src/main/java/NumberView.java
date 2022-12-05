11e9c6fea86ec4cbb8284fc354133269c4205524
PippoZord <pippolor8@gmail.com>
2022-12-05 15:29:38 +0100

    ROSSO: Storia5

---
+++ src/main/java/NumberView.java
@@ -0,0 +1,12 @@
+import org.example.ViewStrategy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class NumberView implements ViewStrategy {
+    @Override
+    public void format(StringBuilder sb, List<Map.Entry<String, Set<String>>> list) {
+
+    }
+}
--- src/test/java/FacebukTest.java
+++ src/test/java/FacebukTest.java
@@ -13,28 +13,36 @@ import static org.assertj.core.api.Assertions.*;
 public class FacebukTest {
 
     private final static String input = "Mario Roberta Luca Filippo\nRoberta Mario Luca Filippo Anna\nLuca Mario Roberta Filippo Anna\nFilippo Mario Roberta Luca Anna\nAnna Roberta Luca Filippo\n";
+
     @Test
-    public void Storia1_2(){
+    public void Storia1_2() {
         Facebuk SUT = new Facebuk(new StringReader(input));
         assertThat(SUT.toString()).isEqualTo("Mario Roberta Luca Filippo \nRoberta Mario Luca Filippo Anna \nLuca Mario Roberta Filippo Anna \nFilippo Mario Roberta Luca Anna \nAnna Roberta Luca Filippo \n");
     }
 
     @Test
-    public void Storia3(){
+    public void Storia3() {
         Facebuk SUT = new Facebuk(new StringReader(input));
         assertThat(SUT.amiciComuni("Filippo", "Anna")).containsExactlyInAnyOrder("Roberta", "Luca");
     }
 
     @Test
-    public void Storia4(){
+    public void Storia4() {
         Facebuk SUT = new Facebuk(new StringReader(input));
         assertThat(SUT.output()).isEqualTo("(Filippo Anna) [Roberta Luca]\n(Luca Anna) [Roberta Filippo]\n(Luca Filippo) [Mario Roberta Anna]\n(Mario Filippo) [Roberta Luca]\n(Mario Luca) [Roberta Filippo]\n(Mario Roberta) [Luca Filippo]\n(Roberta Anna) [Luca Filippo]\n(Roberta Filippo) [Mario Luca Anna]\n(Roberta Luca) [Mario Filippo Anna]\n");
     }
 
     @Test
-    public void StrategyPattern(){
+    public void StrategyPattern() {
         Facebuk SUT = new Facebuk(new StringReader(input));
         SUT.setViewStrategy(new ListView());
         assertThat(SUT.output()).isEqualTo("(Filippo Anna) [Roberta Luca]\n(Luca Anna) [Roberta Filippo]\n(Luca Filippo) [Mario Roberta Anna]\n(Mario Filippo) [Roberta Luca]\n(Mario Luca) [Roberta Filippo]\n(Mario Roberta) [Luca Filippo]\n(Roberta Anna) [Luca Filippo]\n(Roberta Filippo) [Mario Luca Anna]\n(Roberta Luca) [Mario Filippo Anna]\n");
+    }
+
+    @Test
+    public void Storia5() {
+        Facebuk SUT = new Facebuk(new StringReader(input));
+        SUT.setViewStrategy(new NumberView());
+        assertThat(SUT.output()).isEqualTo("(Filippo Anna) 2\n(Luca Anna) 2\n(Luca Filippo) 3\n(Mario Filippo) 2\n(Mario Luca) 2\n(Mario Roberta) 2\n(Roberta Anna) 2\n(Roberta Filippo) 3\n(Roberta Luca) 3\n");
     }
 }
