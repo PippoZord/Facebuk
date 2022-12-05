f08b8e58a441f831ed1b1f8c0de60f0790e9e9d1
PippoZord <pippolor8@gmail.com>
2022-12-05 12:06:10 +0100

    VERDE: ReadFile

--- src/main/java/org/example/Facebuk.java
+++ src/main/java/org/example/Facebuk.java
@@ -1,9 +1,36 @@
 package org.example;
 
 import java.io.StringReader;
+import java.util.*;
 
 public class Facebuk {
-    public Facebuk(StringReader input) {
 
+    private final Map<String, Set<String>> users = new LinkedHashMap<>();
+    public Facebuk(StringReader input) {
+        Scanner s = new Scanner(input);
+        while (s.hasNextLine()){
+            String [] nomi = s.nextLine().split(" ");
+            Set<String> amici = new LinkedHashSet<>();
+            for (int i = 1; i<nomi.length; i++){
+                amici.add(nomi[i]);
+            }
+            users.put(nomi[0], amici);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : users.keySet()){
+            sb.append(s);
+            sb.append(" ");
+            for (String v : users.get(s)){
+                sb.append(v);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
