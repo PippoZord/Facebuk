362b7bfaa5981045e163cb2f8229c92969a6b3f7
PippoZord <pippolor8@gmail.com>
2022-12-05 14:46:22 +0100

    VERDE: Storia4

--- src/main/java/org/example/Facebuk.java
+++ src/main/java/org/example/Facebuk.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.StringReader;
 import java.util.*;
 
 public class Facebuk {
 
     private final Map<String, Set<String>> users = new LinkedHashMap<>();
+    private final Map<String, Set<String>> amiciSet = new TreeMap<>();
+
     public Facebuk(StringReader input) {
         Scanner s = new Scanner(input);
         while (s.hasNextLine()){
@@ -13,7 +17,7 @@ public class Facebuk {
         }
     }
 
-    private void popolaMappa(String s){
+    private void popolaMappa(@NotNull String s){
         String [] nomi = s.split(" ");
         Set<String> amici = new LinkedHashSet<>(Arrays.asList(nomi).subList(1, nomi.length));
         users.put(nomi[0], amici);
@@ -34,8 +38,42 @@ public class Facebuk {
     }
 
     public Set<String> amiciComuni(String u1, String u2) {
-        Set<String> comuni = new LinkedHashSet<>(users.get(u1));
+        Set<String> comuni = new HashSet<>(users.get(u1));
         comuni.retainAll(users.get(u2));
         return comuni;
+    }
+
+    public String output() {
+        for (String key : users.keySet()){
+            for (String v : users.get(key)){
+                if (!key.equals(v) && sonoAmici(key, v)) {
+                    if (!amiciSet.containsKey(v + " " + key)){
+                        amiciSet.put(key + " " + v, amiciComuni(key, v));
+                    }
+                }
+
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (String k: amiciSet.keySet()) {
+            sb.append("(");
+            sb.append(k);
+            sb.append(") ");
+            sb.append("[");
+            for (String v: amiciSet.get(k)) {
+                sb.append(v);
+                sb.append(" ");
+            }
+            sb.deleteCharAt(sb.length()-1);
+            sb.append("]\n");
+        }
+
+        return sb.toString();
+    }
+
+    private boolean sonoAmici(String key, String v) {
+        return users.get(key).contains(v);
     }
 }
