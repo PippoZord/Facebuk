61c53da377fd815a365b6cd450fdb5c583619397
PippoZord <pippolor8@gmail.com>
2022-12-05 12:24:22 +0100

    ROSSO: Storia4

--- src/test/java/FacebukTest.java
+++ src/test/java/FacebukTest.java
@@ -24,4 +24,10 @@ public class FacebukTest {
         assertThat(SUT.amiciComuni("Filippo", "Anna")).containsExactlyInAnyOrder("Roberta", "Luca");
     }
 
+    @Test
+    public void Storia4(){
+        Facebuk SUT = new Facebuk(new StringReader(input));
+        assertThat(SUT.output()).isEqualTo("(Filippo Anna) [Roberta Luca]\n(Luca Anna) [Roberta Filippo]\n(Luca Filippo) [Mario Roberta Anna]\n(Mario Filippo) [Roberta Luca]\n(Mario Luca) [Roberta Filippo]\n(Mario Roberta) [Luca Filippo]\n(Roberta Anna) [Luca Filippo]\n(Roberta Filippo) [Mario Luca Anna]\n(Roberta Luca) [Mario Filippo Anna]\n");
+    }
+
 }
